ec75bebda24dad3346b415cec0098ee003a5bc16
Zheka-ded <11vitamin11@rambler.ru>
2020-12-01 06:01:45 +0200

    unit-10 show description

--- unit-10/project__films/client/src/components/films/FilmCard.js
+++ unit-10/project__films/client/src/components/films/FilmCard.js
@@ -1,34 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from 'prop-types'
 import Featured from './Featured'
 
-const FilmCard = ({film}) => (
-    <div className="ui card">
-        <span className="ui right corner label">
-            <i className="empty star icon" />
-        </span>
-        <div className="image">
-            <span className="ui green label ribbon">$ {film?.price} </span>
-            <Featured featured={film.featured} id={film._id} />
-            <img src={film?.img} alt={film?.title} />
-        </div>
+const FilmCard = ({film}) => {
+    /**
+     * С версткой вообще не заморачивался, да и с логткой походу тоже)))
+     * но теперь могу посмотреть как ты сделал
+     */
+    const [eyeOn, setEyeOn] = useState(true);
 
-        <div className="content">
-            <span href="#" className="header">
-                {film?.title}
+    const toggleDescription = eye => eye(!eyeOn);
+    const eyeCls = eyeOn ? '' : 'slash';
+
+    return (
+        <div className="ui card">
+            <span className="ui right corner label">
+                <i className="empty star icon" />
             </span>
-            <div className="meta">
-                <i className="icon users" /> {film?.director}
-                <span className="right floated">
-                    <i className="icon wait right" /> {film?.duration} min
+            {eyeOn ? (
+                <div className="image">
+                    <span className="ui green label ribbon">$ {film?.price} </span>
+                    <Featured featured={film.featured} id={film._id} />
+                    <img src={film?.img} alt={film?.title} />
+                </div>
+            ) : (
+                <div className="description">
+                    {film?.description}
+                </div>
+            )}
+
+            <div className="content">
+                <span href="#" className="header">
+                    {film?.title}
+                </span>
+                <div className="meta">
+                    <i className="icon users" /> {film?.director}
+                    <span className="right floated">
+                        <i className="icon wait right" /> {film?.duration} min
+                    </span>
+                </div>
+                
+                <span onClick={() => toggleDescription(setEyeOn)}>
+                    <i className={`icon link eye ${eyeCls}`}></i>
                 </span>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 FilmCard.propTypes = {
+
     film: PropTypes.shape({
         title: PropTypes.string.isRequired,
         director: PropTypes.string.isRequired,
@@ -36,7 +58,8 @@ FilmCard.propTypes = {
         duration: PropTypes.number.isRequired,
         price: PropTypes.number.isRequired,
         featured: PropTypes.bool.isRequired,
-    })
+    }),
+    
 }
 
 export default React.memo(FilmCard)
